1bf9fc17615ec05791240c1e1dde36044317d39b
Alvaro Devesa <soy@alv.dev>
2021-07-02 16:31:25 +0200

    Delete flex-hero-header directory

--- flex-hero-header/js/script.js
+++
@@ -1,9 +0,0 @@
-// Burguer Menu
-document.addEventListener('click', () => {
-  const burger = document.querySelector('#burger');
- 
-  burger.classList.contains('hide') ?
-    burger.classList.remove('hide') :
-    burger.classList.add('hide')
-
-});
