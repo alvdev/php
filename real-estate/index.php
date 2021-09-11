29250d5faca3c864ebab680ab2d0b3661b8470b6
alv.dev <soy@alv.dev>
2021-09-11 16:39:16 +0200

    Layout base created

---
+++ real-estate/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Idealisto Real Estate</title>
+    <link rel="stylesheet" href="public/css/style.css">
+</head>
+
+<body>
+    <aside>Aside</aside>
+    <nav>
+        <div class="logo">idealisto</div>
+        <div class="btn-group">
+            <a href="#" class="btn blue">Add property</a>
+            <a href="#" class="btn">Login</a>
+        </div>
+    </nav>
+    <main>Main</main>
+    <footer>
+        Created by alv.dev on <?= date('d-m-Y') ?>
+    </footer>
+</body>
+
+</html>
