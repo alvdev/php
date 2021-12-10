ea55f014ec2c8425c16c086c5a55f3bb2d33b800
alvdev <soy@alv.dev>
2021-12-10 06:05:51 +0100

    Testing objects

--- instagram/index.php
+++ instagram/index.php
@@ -1,9 +1,14 @@
 <?php
 
-use alvdev\Person;
-
 require_once 'vendor/autoload.php';
 
-$person = new Person('Alvaro', 'Devesa');
+use alvdev\Person;
+use alvdev\Student;
 
-echo $person->getPerson();
+$person = new Person('Alvaro', 'Devesa', '6585', '87575');
+$student = new Student('Sandra', 'Pérez', '2227647');
+
+// echo $person->getPerson();
+echo $person;
+echo '<br>';
+echo $student;
